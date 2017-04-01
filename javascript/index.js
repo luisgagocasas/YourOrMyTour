0fdc3577cb79be31f38e59c8ba675e4e8c5fa5a9
Luis Gago Casas <luisgago@lagc-peru.com>
2017-04-01 12:03:13 -0500

    nuevo contenido

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-    $('#escribenos').ajaxForm(function() {
-        //Validacion
+    //Paquete personalizado
+    $("#personaID").on( "change", function() {
+        var cantidad = $("#personaID").val();
+        var dolares = $("#valorDolares").attr("data-dolares");
+        var soles = $("#valorSoles").attr("data-soles");
+        var valFinalDolares = "$ " + cantidad * dolares;
+        var valFinalSoles = "S/. " + cantidad * soles;
+        
+        $("[name=dolares]").val(valFinalDolares);
+        $("[name=soles]").val(valFinalSoles);
+        
+        $("#valorDolares").text(valFinalDolares);
+        $("#valorSoles").text(valFinalSoles);
+    });
+    
+    function validate1(formData, jqForm, options) {
         var nombreValue = $('input[name=nombre]').fieldValue();
         var correoValue = $('input[name=correo]').fieldValue();
         if (!nombreValue[0] || !correoValue[0]) {
@@ -8,23 +22,42 @@ $(document).ready(function(){
             return false;
         }
         else {
+            $("#mensaje-stado").show();
+        }
+    }
+    function validate2(formData, jqForm, options) {
+        var nombreValue = $('input[name=nombre]').fieldValue();
+        var correoValue = $('input[name=correo]').fieldValue();
+        var dniValue = $('input[name=dni]').fieldValue();
+        if (!nombreValue[0] || !correoValue[0]) {
+            alert('Es requerido los campos Nombre, Correo y DNI');
+            return false;
+        }
+        else {
+            $("#mensaje-stado").show();
+        }
+    }
+    
+    $('#paquete').ajaxForm({
+        beforeSubmit: validate2,
+        success: function() {
             console.log("enviando");
-            $("#escribenos")[0].reset();
+            $("#paquete")[0].reset();
             $("#mensaje-stado .form-mensaje-text").text("Mensaje Enviado :)");
             setTimeout(function(){
                 $("#mensaje-stado").hide();
             }, 3000);
         }
     });
-    $("#enviarForm").on( "click", function() {
-        $("#mensaje-stado").show();
-    });
-    $("#personaID").on( "change", function() {
-        var cantidad = $("#personaID").val();
-        var dolares = $("#valorDolares").attr("data-dolares");
-        var soles = $("#valorSoles").attr("data-soles");
-        $("#valorDolares").text("$" + cantidad * dolares);
-        $("#valorSoles").text("S/. " + cantidad * soles);
-        
+    //Escribenos
+    $('#escribenos').ajaxForm({
+        beforeSubmit: validate1,
+        success: function() {
+            $("#escribenos")[0].reset();
+            $("#mensaje-stado .form-mensaje-text").text("Mensaje Enviado :)");
+            setTimeout(function(){
+                $("#mensaje-stado").hide();
+            }, 3000);
+        }
     });
 });
--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-    $("#escribenos").ajaxForm(function() {
+    function validate1(formData, jqForm, options) {
         var nombreValue = $("input[name=nombre]").fieldValue(), correoValue = $("input[name=correo]").fieldValue();
         if (!nombreValue[0] || !correoValue[0]) return alert("Es requerido los campos Nombre y Correo"), 
         !1;
-        console.log("enviando"), $("#escribenos")[0].reset(), $("#mensaje-stado .form-mensaje-text").text("Mensaje Enviado :)"), 
-        setTimeout(function() {
-            $("#mensaje-stado").hide();
-        }, 3e3);
-    }), $("#enviarForm").on("click", function() {
         $("#mensaje-stado").show();
-    }), $("#personaID").on("change", function() {
-        var cantidad = $("#personaID").val(), dolares = $("#valorDolares").attr("data-dolares"), soles = $("#valorSoles").attr("data-soles");
-        $("#valorDolares").text("$" + cantidad * dolares), $("#valorSoles").text("S/. " + cantidad * soles);
+    }
+    function validate2(formData, jqForm, options) {
+        var nombreValue = $("input[name=nombre]").fieldValue(), correoValue = $("input[name=correo]").fieldValue();
+        $("input[name=dni]").fieldValue();
+        if (!nombreValue[0] || !correoValue[0]) return alert("Es requerido los campos Nombre, Correo y DNI"), 
+        !1;
+        $("#mensaje-stado").show();
+    }
+    $("#personaID").on("change", function() {
+        var cantidad = $("#personaID").val(), dolares = $("#valorDolares").attr("data-dolares"), soles = $("#valorSoles").attr("data-soles"), valFinalDolares = "$ " + cantidad * dolares, valFinalSoles = "S/. " + cantidad * soles;
+        $("[name=dolares]").val(valFinalDolares), $("[name=soles]").val(valFinalSoles), 
+        $("#valorDolares").text(valFinalDolares), $("#valorSoles").text(valFinalSoles);
+    }), $("#paquete").ajaxForm({
+        beforeSubmit: validate2,
+        success: function() {
+            console.log("enviando"), $("#paquete")[0].reset(), $("#mensaje-stado .form-mensaje-text").text("Mensaje Enviado :)"), 
+            setTimeout(function() {
+                $("#mensaje-stado").hide();
+            }, 3e3);
+        }
+    }), $("#escribenos").ajaxForm({
+        beforeSubmit: validate1,
+        success: function() {
+            $("#escribenos")[0].reset(), $("#mensaje-stado .form-mensaje-text").text("Mensaje Enviado :)"), 
+            setTimeout(function() {
+                $("#mensaje-stado").hide();
+            }, 3e3);
+        }
     });
 });
